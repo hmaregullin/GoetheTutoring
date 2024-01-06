5cd16355fdbc49626ce8a399c31a16434a3ed4b1
Hector Miguel Reynaga Aregullin <hmaregullin@gmail.com>
2024-01-05 21:43:21 -0600

    first commit

---
+++ src/components/MainPage.vue
@@ -0,0 +1,30 @@
+<template>
+    <h1>Goethe Tutoring</h1>
+    <p>A guide for everyone who wants to get a good grade in Goethe</p>
+
+    <h2>Why study german?</h2>
+    <p>Learning the language not only enhances your communication skills but also opens doors to numerous opportunities, particularly if you aim to work for prestigious German companies like SAP, Volkswagen, or BMW. Proficiency in German offers access to high-quality education, enriches your career prospects, unlocks various business opportunities, and provides a deeper appreciation for the rich cultural heritage of Germany and German-speaking regions.</p>
+
+    <h3>What is the Goethe-Zertifikat?</h3>
+    <p>The Goethe Zertifikat is a certification that attests to your proficiency in the German language, guided by the Common European Framework of Reference (CEFR).</p>
+
+    <h3>Who Needs It?</h3>
+    <p>This certification is essential for individuals interested in pursuing studies in Germany or any other country where German is an official language (such as Austria, Switzerland, or Liechtenstein). Holding this certification can significantly enhance your employment prospects and open up opportunities for international travel.</p>
+
+    <h3>Tips Before Taking the Exam</h3>
+    <ul>
+      <li>Verify that the examination location is officially recognized. Confirm its authenticity by clicking <a href="https://www.goethe.de/en/wwt.html">here</a>.</li>
+      <li>Identify your weaknesses and dedicate extra effort to improve in those areas.</li>
+      <li>Ensure your mobile phone is turned off. Examiners may cancel your exam if your mobile emits any sound.</li>
+      <li>Show respect and arrive on time.</li>
+    </ul>
+
+    <h3>Ready? Select one of the guides</h3>
+
+</template>
+
+<script>
+export default {
+  name: 'MainPage',
+}
+</script>
